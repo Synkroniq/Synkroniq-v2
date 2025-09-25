f246cbb9faf19936833cc91220668bf443cbdd60
SYNKRONIQ <eduachou@gmail.com>
2025-09-25 00:46:30 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Synkroniq 2.0 iniciado");
+
+  // Carrega o cabeçalho
+  fetch("components/header.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("header-container").innerHTML = html;
+    });
+
+  // Carrega o rodapé
+  fetch("components/footer.html")
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById("footer-container").innerHTML = html;
+    });
 });
