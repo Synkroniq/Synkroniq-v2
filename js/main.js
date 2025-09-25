76b943a13217488693f055102138dc535b5db947
SYNKRONIQ <eduachou@gmail.com>
2025-09-25 19:20:58 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.text())
     .then(html => {
       document.getElementById("header-container").innerHTML = html;
-    });
+    })
+    .catch(err => console.error("Erro ao carregar o cabeçalho:", err));
 
   // Carrega o rodapé
   fetch("components/footer.html")
     .then(res => res.text())
     .then(html => {
       document.getElementById("footer-container").innerHTML = html;
-    });
+    })
+    .catch(err => console.error("Erro ao carregar o rodapé:", err));
 });
